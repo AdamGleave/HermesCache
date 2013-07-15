5a2956ba3767b09a9f96317a180f88f5c0510405
id <id@saaj.me>
2013-07-15 13:10:41 +0400

    .__self__ note

--- hermes/__init__.py
+++ hermes/__init__.py
@@ -187,6 +187,8 @@ class Cached(object):
     # instance method.
     self._callable = types.MethodType(self._fn, instance, owner)
     
+    # Intance can also be calculated through self._callable.__self__, 
+    # however I find the following way more convinient.
     result            = functools.partial(self.__call__,   instance)
     result.invalidate = functools.partial(self.invalidate, instance)
     
