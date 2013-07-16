1d02fca46a1346038a588dc278e7b83eaeab1f63
id <id@saaj.me>
2013-07-16 19:17:58 +0400

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name             = 'HermesCache',
-  version          = '0.0.1',
+  version          = '0.1.0',
   author           = 'saaj',
   author_email     = 'mail@saaj.me',
   packages         = ['hermes', 'hermes.test'],
