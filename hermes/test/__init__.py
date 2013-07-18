aa9c9fa832f175ed142cbd5b8b43b1414c694338
id <id@saaj.me>
2013-07-18 16:07:30 +0400

    add readme example case

--- hermes/test/__init__.py
+++ hermes/test/__init__.py
@@ -61,3 +61,41 @@ def createFixture(cache):
       return {'a' : a['alpha'], 'b' : {'b' : b[0]}}
     
   return Fixture()
+
+
+class TestReadme(unittest.TestCase):
+  
+  def testExample(self):
+    import hermes.backend.redis
+
+  
+    cache = hermes.Hermes(hermes.backend.redis.Backend, ttl = 600, host = 'localhost', db = 1)
+        
+    @cache
+    def foo(a, b):
+      return a * b
+    
+    class Example:
+          
+      @cache(tags = ('math', 'power'), ttl = 1200)
+      def bar(self, a, b):
+        return a ** b
+        
+      @cache(tags = ('math', 'avg'), key = lambda fn, *args, **kwargs: 'avg:{0}:{1}'.format(*args[1:]))
+      def baz(self, a, b):
+        return (a + b) / 2.0
+
+          
+    print foo(2, 333)
+    
+    example = Example()
+    print example.bar(2, 10)
+    print example.baz(2, 10)
+        
+    foo.invalidate(2, 333)
+    example.bar.invalidate(2, 10)
+    example.baz.invalidate(2, 10)
+        
+    cache.clean(['math']) # invalidate entries tagged 'math'
+    cache.clean()         # flush cache
+    
