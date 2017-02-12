c9681f5c78fa2b290db5dc990f5ba9ecbacd9c49
saaj <id@saaj.me>
2017-02-12 20:12:27 +0100

    Minor improvement.

--- hermes/backend/redis.py
+++ hermes/backend/redis.py
@@ -3,10 +3,11 @@
 '''
 
 
-import time
-import importlib
+from __future__ import absolute_import
 
-redis = importlib.import_module('redis')
+import time
+
+import redis
 
 from . import AbstractBackend, AbstractLock
 
@@ -15,27 +16,27 @@ __all__ = 'Lock', 'Backend'
 
 
 class Lock(AbstractLock):
-  '''Key-aware distributed lock. "Distributed" is in sense of clients, 
-  not Redis instances. Implemented as described `here 
+  '''Key-aware distributed lock. "Distributed" is in sense of clients,
+  not Redis instances. Implemented as described `here
   <http://redis.io/topics/distlock#correct-implementation-with-a-single-instance>`_,
-  but without setting unique value to the lock entry and later checking it, 
+  but without setting unique value to the lock entry and later checking it,
   because it is expected for a cached function to comlete before lock timeout.'''
-  
+
   client = None
   '''Redis client'''
-  
+
   timeout = 900
   '''Maximum TTL of lock'''
-  
+
   sleep = 0.1
   '''Amount of time to sleep per ``while True`` iteration when waiting'''
-  
-  
+
+
   def __init__(self, key, client, **kwargs):
     super(Lock, self).__init__(key)
-    
+
     self.client = client
-    
+
     self.sleep   = kwargs.get('lockSleep',   self.sleep)
     self.timeout = kwargs.get('lockTimeout', self.timeout)
     if self.timeout is None:
@@ -56,34 +57,34 @@ class Lock(AbstractLock):
 
 class Backend(AbstractBackend):
   '''Redis backend implementation'''
-  
+
   client = None
   '''Redis client'''
-  
+
   _options = None
   '''Lock options'''
-  
-  
+
+
   def __init__(self, mangler, **kwargs):
     super(Backend, self).__init__(mangler)
-    
+
     self.client = redis.StrictRedis(**{
       'host'     : kwargs.pop('host',     'localhost'),
       'password' : kwargs.pop('password', None),
       'port'     : kwargs.pop('port',     6379),
       'db'       : kwargs.pop('db',       0)
     })
-    
+
     self._options = kwargs
-  
+
   def lock(self, key):
     return Lock(self.mangler.nameLock(key), self.client, **self._options)
-  
+
   def save(self, key = None, value = None, mapping = None, ttl = None):
     if not mapping:
       mapping = {key : value}
     mapping = {k : self.mangler.dumps(v) for k, v in mapping.items()}
-    
+
     if not ttl:
       self.client.mset(mapping)
     else:
@@ -91,7 +92,7 @@ class Backend(AbstractBackend):
       for k, v in mapping.items():
         pipeline.setex(k, ttl, v)
       pipeline.execute()
-  
+
   def load(self, keys):
     if self._isScalar(keys):
       value = self.client.get(keys)
@@ -100,12 +101,13 @@ class Backend(AbstractBackend):
       return value
     else:
       keys = tuple(keys) # py3 compat
-      return {k : self.mangler.loads(v) for k, v in zip(keys, self.client.mget(keys)) if v is not None}
-  
+      return {k : self.mangler.loads(v)
+        for k, v in zip(keys, self.client.mget(keys)) if v is not None}
+
   def remove(self, keys):
     if self._isScalar(keys):
       keys = (keys,)
-      
+
     self.client.delete(*keys)
 
   def clean(self):
