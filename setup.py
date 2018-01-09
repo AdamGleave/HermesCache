d7486d7c29d9cd8ec83ded143017ddde6897300b
saaj <id@saaj.me>
2018-01-09 18:50:59 +0100

    Increment patch version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
   name         = 'HermesCache',
-  version      = '0.7.2',
+  version      = '0.7.3',
   author       = 'saaj',
   author_email = 'mail@saaj.me',
   packages     = ['hermes', 'hermes.backend', 'hermes.test'],
